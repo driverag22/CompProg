42cf0949f6c2bda7068952e86b0932b14f4fa648
Diego Rivera <driverag22@gmail.com>
2022-09-21 15:13:37 +0200

    wiring almost working

--- in_process/badwiring/wiring.cpp
+++ in_process/badwiring/wiring.cpp
@@ -1,6 +1,6 @@
-#include <bits/stdc++.h>
-#include <ostream>
-#include <vector>
+#include "bits/stdc++.h"
+#include "ostream"
+#include "vector"
 using namespace std;
 typedef long long ll;
 
@@ -8,60 +8,53 @@ int n, D, counter;
 int l [105];
 vector<int> ans;
 
-void flip(int s [105], int a){
-	int p = min(a+D, n-1);
-	int q = max(a-D, 0);
-	for(int i = a; i <= p; i++) (s[i] == 0) ? s[i] = 1 : s[i] = 0;
-	for(int i = q; i < a; i++) (s[i] == 0) ? s[i] = 1 : s[i] = 0;
+void flip(int s [105], int index){
+	int p = min(index+D, n-1);
+	int q = max(index-D, 0);
+    for(int i = q; i <= p; i++) (s[i] == 0) ? s[i] = 1 : s[i] = 0;
 }
-
-bool checkPast(int s [105], int t){
-	if(t <= D) return true;
-	for(int i = 0; i < t-D; i++){
-		if (s[i] == 1) return false;
-	}
-
-	return true;
+bool allOff(int s [105], int n){
+    for(int i = 0; i < n; i++){
+        if(s[i] == 1) return false;
+    }
+    return true;
 }
-
-bool allOff(int s [105]){
-	bool a = true;
-	for(int i = 0; i < n; i++){
-		if(s[n] == 1) a = false;
-		return a;
-	}
-	return a;
+bool prevFine(int s [105], int index, int D){
+    if (index <= D) return true;
+    for (int i = 0; i < index-D; i++) {
+        if(s[i] == 1) return false;
+    }
+    return true;
 }
+void recurse(int s [105], int index, int c, int n){
+    if(allOff(s, n)) ans.push_back(c);
+//    if(index > D && s[index-D-1] == 1) return;
+    if(!prevFine(s, index, D)) return;
+	if(index == n) return;
 
-void recurse(int s [105], int t, ll c, int n){
-	if (allOff(s)) ans.push_back(c);
-	if(!checkPast(s, t)) return;
-	if(t == n) ans.push_back(c);	
-	
-	for(int i = 0; i < n; i++) {
-		recurse(s, t+1, c, n);
-		flip(s, i); 
-		recurse(s, t+1, c+1, n);
-	}
+    recurse(s, index+1, c, n);
+    flip(s, index);
+    recurse(s, index+1, c+1, n);
 }
 
 void oneRun(){
 	cin >> n >> D;
 
 	for(int i = 0; i < n; i++) cin >> l[i];
-	ll c = 0;
-	recurse(l, 0, c, n);
+	recurse(l, 0, 0, n);
 
-	vector<int>::const_iterator it = ans.begin();	
-	int min = *(it++);
+    if(ans.size() == 0) cout << "impossible" << endl;
+    else {
+        vector<int>::const_iterator it = ans.begin();
+        int min = *(it++);
 
-	while(it != ans.end()){
-		if (*it < min) min = *it; 
-		++it;
-	}
-	if(ans.size() == 0) cout << "impossible" << endl;
-	else cout << min << endl; 
-
+        while (it != ans.end()) {
+            if (*it < min) min = *it;
+            ++it;
+        }
+        cout << min << endl;
+        ans.clear();
+    }
 }
 
 int main(){
