f87ee0232265923795298169ce14e3cdad2ed182
diego <driverag22@gmail.com>
2022-09-21 12:00:54 +0200

    better wiring

--- in_process/badwiring/wiring.cpp
+++ in_process/badwiring/wiring.cpp
@@ -1,15 +1,26 @@
 #include <bits/stdc++.h>
 #include <ostream>
+#include <vector>
 using namespace std;
 
 int n, D, counter;
 int l [105];
+vector<int> ans;
 
 void flip(int s [105], int a){
 	int p = min(a+D, n-1);
 	int q = max(a-D, 0);
 	for(int i = a; i <= p; i++) (s[i] == 0) ? s[i] = 1 : s[i] = 0;
 	for(int i = q; i < a; i++) (s[i] == 0) ? s[i] = 1 : s[i] = 0;
+}
+
+bool checkPast(int s [105], int t){
+	if(t <= D) return true;
+	for(int i = 0; i < t-D; i++){
+		if (s[i] == 1) return false;
+	}
+
+	return true;
 }
 
 bool allOff(int s [105]){
@@ -21,17 +32,16 @@ bool allOff(int s [105]){
 	return a;
 }
 
-int recurse(int s [105], int t, int c, int n){
-	if (allOff(s)) return c;
-	if(t == n) return -1;	
+void recurse(int s [105], int t, int c, int n){
+	if (allOff(s)) ans.push_back(c);
+	if(!checkPast(s, t)) return;
+	if(t == n) ans.push_back(c);	
 	
-	int temp [n];
-	for(int i = 0; i < n; i++) temp[i] = s[i];
 	for(int i = 0; i < n; i++) {
-		if(s[i] == 1) flip(temp, i);
-		return min(recurse(temp, t+1, c+1, n), recurse(l, t+1, c+1, n));
+		recurse(s, t+1, c, n);
+		flip(s, i); 
+		recurse(s, t+1, c+1, n);
 	}
-	return 69;
 }
 
 void oneRun(){
@@ -39,10 +49,16 @@ void oneRun(){
 
 	for(int i = 0; i < n; i++) cin >> l[i];
 
+	vector<int>::const_iterator it = ans.begin();	
+	int min = *(it++);
 
-	counter = recurse(l, 0, 0, n);
+	while(it != ans.end()){
+		if (*it < min) min = *it; 
+		++it;
+	}
+	if(ans.size() == 0) cout << "impossible" << endl;
+	else cout << min << endl; 
 
-	(counter == -1) ? cout << "impossible" << endl : cout << counter << endl;
 }
 
 int main(){
