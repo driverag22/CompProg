e57dc6e7788c9de8cddaea35729d8f3421c87f2d
diego <driverag22@gmail.com>
2022-12-02 15:55:18 +0100

    solved secret santa

---
+++ solved/SecretSanta/secretSanta.cpp
@@ -0,0 +1,38 @@
+#include "iostream"
+#include <cstdio>
+#include <bits/stdc++.h>
+#include "vector"
+using namespace std;
+
+int factorial (int t) {
+    int fact = 1;
+    for (int i = 2; i <= t; i++) fact *= i;
+    return fact;
+}
+int main(){
+
+    int N;
+    cin >> N;
+
+    if (N < 10) {
+        double sum = 1.0;
+        int fact;
+        for (int i = 2; i <= N; i+=2) {
+            fact = factorial(i);
+            sum += (1.0 / fact);
+        }
+        for (int i = 1; i <= N; i+=2) {
+            fact = factorial(i);
+            sum -= (1.0/fact);
+        }
+        sum = 1 - sum;
+
+        cout << sum << endl;
+    } else {
+        double a = 0.632121;
+        cout << a << endl;
+    }
+
+
+    return 0;
+}
