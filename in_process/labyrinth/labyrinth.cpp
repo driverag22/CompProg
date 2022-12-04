126d8b62cdb8f0ba994b3f8cf97f1f137d4d9ef8
diego <driverag22@gmail.com>
2022-12-04 23:30:51 +0100

    cleared up labyrinth, still fails

--- in_process/labyrinth/labyrinth.cpp
+++ in_process/labyrinth/labyrinth.cpp
@@ -1,9 +1,8 @@
-#include "iostream"
+#include <iostream>
 #include <cstdio>
 #include <bits/stdc++.h>
 #include "vector"
 using namespace std;
-int h, w; 
 double counter;
 int init_x, init_y;
 int dx[4] = {1, 0, -1, 0};
@@ -14,46 +13,52 @@ struct Node{
 	int visited = 0;
 };
 
-Node nodes[207][207];
 
-void DFS(int x, int y){
+void DFS(int x, int y, Node nodes[201][201]) {
 	if (nodes[x][y].visited == 1 || nodes[x][y].type == '#'){
 		return;
 	}
-	if (nodes[x][y].type == '.' || nodes[x][y].type == 't'){
+	if (nodes[x][y].type == '.'){
 		counter++;
 	} 
+    if (nodes[x][y].type == 't'){
+        counter++;
+        return;
+    }
 	nodes[x][y].visited = 1;
     for (int i = 0; i < 4; i++){
-		DFS(x + dx[i], y + dy[i]);
+		DFS(x + dx[i], y + dy[i], nodes);
 	}
 }
 
 void oneRun(){
+    int h,w;
     cin >> h >> w;
+    Node nodes[201][201];
 	counter = 0;
-
-    for(int i = 1; i <= h; i++){
+    // nodes[width][height]
+    for(int y = 0; y < h; y++){
         string s;
         cin >> s;
-        for(int j = 1; j <= w; j++){
-            nodes[i][j].type = s[j];
-            nodes[i][j].visited = 0;
-			if(s[j] == 's'){
-                init_x = i; init_y = j;
+        for(int x = 0; x < w; x++){
+            nodes[x][y].type = s[x];
+            nodes[x][y].visited = 0;
+			if(s[x] == 's'){
+                init_y = y; init_x = x;
             }
         }
     }
-    for(int i = 0; i <= w+2; i++){
-        nodes[0][i].visited = '#';
-        nodes[h+2][i].visited = '#';
+    cout << endl;
+    cout << endl;
+    for(int y = 0; y < h; y++){
+        for(int x = 0; x<w;x++){
+            cout << nodes[x][y].type;
+        }
+        cout << endl;
     }
-    for(int i = 0; i <= h+2; i++){
-        nodes[i][0].visited = '#';
-        nodes[i][w+2].visited = '#';
-    }
-
-	DFS(init_x, init_y);
+    cout << endl;
+    cout << endl;
+	DFS(init_x, init_y, nodes);
     printf("%.2f \n", counter);
 }
 
