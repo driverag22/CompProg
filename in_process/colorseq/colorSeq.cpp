f104daee50018beed7debcec77b2dadf8ce41d0b
diego <driverag22@gmail.com>
2022-12-04 23:31:26 +0100

    need to start colorseq, annoying taking input

---
+++ in_process/colorseq/colorSeq.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <cstdio>
+#include <bits/stdc++.h>
+#include <vector>
+using namespace std;
+
+void oneRun(){
+
+}
+
+int main(){
+
+    int cases;
+    cin >> cases;
+
+    while(cases-- > 0){
+        oneRun();
+    }
+
+    return 0;
+}
