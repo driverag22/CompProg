8fb0b65c88dc2f0ed8e279238eabe74d04e8b1e8
diego <driverag22@gmail.com>
2023-02-21 21:42:24 +0100

    started wrongAnswer

---
+++ in_process/wrongAnswer/wrongAnswer.cpp
@@ -0,0 +1,20 @@
+#include "iostream"
+#include <algorithm>
+#include <climits>
+#include <cstdio>
+#include <bits/stdc++.h>
+#include "vector"
+using namespace std;
+
+void oneRun(){
+    int H, V, i, x, y, j;
+    string word;
+    cin >> H >> V;
+    char board[H][V];
+    memset(board, 0, H * V * sizeof(board[0][0]));
+}
+int main() {
+    int cases; cin >> cases;
+    while(cases-- > 0) oneRun(); 
+    return 0;
+}
