43e5b75e08219bd48cc4908f42f6935e0cf01dbe
diego <driverag22@gmail.com>
2023-02-27 10:38:03 +0100

    formula still not working

--- in_process/digitSum/digitSum.cpp
+++ in_process/digitSum/digitSum.cpp
@@ -5,13 +5,11 @@
 #include <bits/stdc++.h>
 #include "vector"
 using namespace std;
-// https://math.stackexchange.com/questions/1991476/sum-of-all-the-digits-of-the-integers-from-one-integer-to-another
-int sumOfDigitsUntil(int n) {
-    int sum = 0;
+long long sumOfDigitsUntil(int n) {
+    long long sum = 0;
     if (n < 0) return 0;
-    else if (n == 1) return 1;
-    for (int i = 0; i < ceil(log10(n)); i++) {
-        int k = floor( ( n - floor( n / pow(10, i+1) ) * pow(10, i+1) ) / pow(10, i) );
+    for (int i = 0; i <= ceil(log10(n)); i++) {
+        long long k = floor( ( n - floor( n / pow(10,i+1) ) * pow(10,i+1) ) / pow(10,i) );
         sum += 45 * pow(10, i) * floor( n / pow(10, i+1) );
         sum += pow(10, i) * ( k * (k-1) ) / 2;
         sum += k * (n + 1 - pow(10, i) * floor( n / pow(10,i) ) );
@@ -20,12 +18,10 @@ int sumOfDigitsUntil(int n) {
 }
 
 void oneRun(){
-    int a, b, sum;
-    /* cin >> a >> b; */
-    cin >> a;
-    /* sum = sumOfDigitsUntil(b) - sumOfDigitsUntil(a-1); */
-    /* cout << sum << endl; */
-    cout << sumOfDigitsUntil(a) << endl;
+    long long sum, a, b;
+    cin >> a >> b;
+    sum = sumOfDigitsUntil(b) - sumOfDigitsUntil(a-1);
+    cout << sum << endl;
 }
 int main() {
     int cases; cin >> cases;
