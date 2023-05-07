eff8f225d1957bfdffff58cfb7a66add0f4a325c
diego <driverag22@gmail.com>
2023-05-07 17:26:07 +0200

    digitSum too slow

--- in_process/digitSum/digitSum.cpp
+++ in_process/digitSum/digitSum.cpp
@@ -6,29 +6,30 @@
 #include "vector"
 using namespace std;
 typedef long long ll;
-ll sumOfDigits(ll n) {
-    if (n < 10) return (n*(n+1))/2;
 
-    ll d = (ll)(log10(n));
-
-    ll *a = new ll[d+1];
-    a[0]=0; a[1]=45;
-    for (ll i = 2; i <= d; i++) 
-        a[i] = a[i-1]*10 + 45*ceil(pow(10,i-1));
-
-    ll p = ceil(pow(10, d));
-    ll msd = n / p;
-
-    return msd*a[d] + (msd*(msd-1)/2)*p + 
-        msd*(1+n%p) + sumOfDigits(n%p);
+int sumDigits(int n) {
+    int sum = 0, m;
+    while (n > 0) {
+        m = n % 10;
+        sum += m;
+        n /= 10;
+    }
+    return sum;
 }
 
-void oneRun(){
-    ll sum, a, b;
+int solve(int n) {
+    if (n <= 0) return 0;
+    else if (n % 10 == 0) return (10 * solve(n/10) + 45 * (n / 10));
+    return solve(n - 1) + sumDigits(n-1);
+}
+
+void oneRun() {
+    int a, b, res;
     cin >> a >> b;
-    sum = sumOfDigits(b) - sumOfDigits(a-1);
-    cout << sum << endl;
+    res = solve(b+1) - solve(a);
+    cout << res << endl;
 }
+
 int main() {
     int cases; cin >> cases;
     while(cases-- > 0) oneRun(); 
